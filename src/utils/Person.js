aa22bbe484d09f2955c444503472640b017d226c
Ashsudheer <ashwinsudheer@outlook.com>
2021-05-16 08:26:42 +0400

    Test Payment Gateway added

--- src/utils/Person.js
+++ src/utils/Person.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Person(props){
-    return (
-        <div className="person">
-            <div className="box1">
-                <p className="name">{props.name}</p>
-                <p>{props.text}</p>
-            </div>
+function Person(props) {
+  return (
+    <div className="person">
+      <div className="content">
+        <p className="name">{props.name}</p>
+        <p className="body">{props.text}</p>
+      </div>
 
-            <img alt="person" src={props.img}></img>
-        </div>
-    );
+      <img className="person-image" alt="person" src={props.img}></img>
+    </div>
+  );
 }
 
 export default Person;
